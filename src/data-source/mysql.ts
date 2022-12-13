205e816943f4c9fe00347797032971321f846bd5
Carlos Rivas <66936404+Carlosr08@users.noreply.github.com>
2022-12-13 16:03:41 -0400

    cambios en conexion de db

--- src/data-source/mysql.ts
+++ src/data-source/mysql.ts
@@ -4,11 +4,11 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "../config";
 
 export async function connect(): Promise<Pool> {
   const connection = await createPool({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
+    user: 'root',
+    password: 'OTJVS2vtQnTikEFi8uym',
+    host: 'containers-us-west-104.railway.app',
+    port: '3306',
+    database: 'railway',
   });
   return connection;
 }
